6ef8a60c65e5e32c5317f262f8e248c94e51d44e
Industria 4.0 <ivandilarios@opendeusto.es>
2021-04-26 17:17:13 +0200

    modelos de datos

--- myapp/models.py
+++ myapp/models.py
@@ -1,37 +1,41 @@
 from django.db import models
 
 # Create your models here.
-
-class Empresa(models.Model):
-    nombre = models.CharField(max_length=50)
-    cif = models.CharField(max_length=12)
-    
-    def __str__(self):
-        return self.nombre
-class Trabajador(models.Model):
-    # Campo para la relación
-    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=40)
-    fecha_nacimiento = models.DateField()
-    # Es posible indicar un valor por defecto mediante 'default'
-    antiguedad = models.IntegerField(default=0)
-    def __str__(self):
-        return self.nombre
-class Red(models.Model):
-    ip = models.CharField(max_length=15)
-    nombre = models.CharField(max_length=200)
-    fecha_creacion = models.DateTimeField('Fecha de creacion')
-
-    def __str__(self):
-        return self.nombre
-
 class Equipo(models.Model):
-    red = models.ForeignKey(Red, on_delete=models.CASCADE)
-    tipo = models.CharField(max_length=30, default='')
-    # Es posible indicar un valor por defecto mediante 'default'
-    votes = models.IntegerField(default=0)
-
+    numerodeserie = models.IntegerField 
+    modelo = models.CharField(max_length=50)
+    marca = models.CharField(max_length =50)
+    tipoequipo = models.CharField(max_length =50)
+    fecha_adquisicion = models.DateField()
+    fecha_puestaenmarcha = models.DateField()
+    proveedor_nombre = models.CharField(max_length =50)
+    proveedor_tlf = models.IntegerField
+    planta = models.CharField(max_length=50)
     def __str__(self):
-        return self.tipo
+        return self.nombre
+        
+class Ticket(models.Model):
+   numeroreferencia = models.IntegerField
+   titulo = models.CharField(max_length =50)
+   descripcion = models.TextField(max_length =1000)
+   fecha_apertura = models.DateField()
+   fecha_resolucion = models.Datefield()
+   nivel_urgencia = models.CharField(max_length = 50)
+   tipo_ticket = models.CharField(max_length=50)
+   estado_ticket = models.CharField(max_length=50)
+   comentario = models.TextField(max_length =1000)
+   FK_Equipo_ID = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+   FK_Empleado_ID = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=25)
+    apellidos = models.CharField(max_length=50)
+    email= models.CharField(max_length=50)
+    telefono = models.IntegerField
+    dni = models.CharField(max_length=9)
+    def __str__(self):
+        return self.nombre
 
 
