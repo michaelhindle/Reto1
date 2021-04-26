c1277876276f040a564be6964468caa1592a1eb5
Michael Hindle <michael.hindle@opendeusto.es>
2021-04-26 17:41:26 +0200

    admin actualizado

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Red, Equipo, Empresa, Trabajador
+from .models import Equipo, Ticket, Empleado
 
 # Register your models here.
-admin.site.register(Empresa)
-admin.site.register(Trabajador)
-admin.site.register(Red)
+admin.site.register(Ticket)
+admin.site.register(Empleado)
 admin.site.register(Equipo)
